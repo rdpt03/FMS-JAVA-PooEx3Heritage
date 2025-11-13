2351f866a46875a1dba06e262e183857a0554f85
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-11-13 09:10:58 +0100

    ex3.4 add min_wage and condition

--- fr/fms/entities/Commercial.java
+++ fr/fms/entities/Commercial.java
@@ -4,10 +4,14 @@ public class Commercial extends Person{
 
 	private String entreprise;
 	private double comission;
+	private static double MIN_WAGE = 0.0;
 	
 	
-	public Commercial(String lastName, String firstName, int age, String adress, City bornCity, String entreprise, double comission) {
+	public Commercial(String lastName, String firstName, int age, String adress, City bornCity, String entreprise, double comission) throws Exception {
 		super(lastName, firstName, age, adress, bornCity);
+		if(comission < MIN_WAGE) {
+			throw new Exception("Veuillez mettre une comission plus grande que "+MIN_WAGE);
+		}
 		// TODO Auto-generated constructor stub
 		this.entreprise = entreprise;
 		this.comission = comission;
@@ -32,7 +36,10 @@ public class Commercial extends Person{
 	}
 
 
-	public void setComission(double comission) {
+	public void setComission(double comission) throws Exception {
+		if(comission < MIN_WAGE) {
+			throw new Exception("Veuillez mettre une comission plus grande que "+MIN_WAGE);
+		}
 		this.comission = comission;
 	}
 
